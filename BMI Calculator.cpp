d9d7c431d8ab13e54b095a712c1673f2e438468a
Salim-Shaik <88785066+Salim-Shaik@users.noreply.github.com>
2022-05-12 20:55:35 +0200

    Add files via upload

---
+++ BMI Calculator.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    double weight=0, height=0;
+    cout<< "Enter Height\n";
+    cin >> height;
+    cout<< "Enter Weight\n";
+    cin>> weight;
+    double bmi = weight/(height*height);
+    if (bmi<18.5)
+        {
+            cout<< "You are under weight: " << bmi.precision(3);
+    }
+    else if(bmi>=18.5 && bmi<=25)
+        {
+            cout<< "Your age average";
+    }
+    else if(bmi>25)
+        {
+            cout<< "obese";
+    }
+    else
+        {
+            cout<<"utc";
+    }
+    return 0;
+}
+
